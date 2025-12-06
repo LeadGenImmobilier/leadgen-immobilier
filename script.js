b0d976decb165e2e2cad5c17d5278197451a12e4
LeadGenImmobilier <leadgenimmobilier@gmail.com>
2025-12-06 15:39:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
 const SUPABASE_URL = 'https://drkjmtanzqmjgpltjqcg.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_N8SmY7U4KDsqCtuoMIQ2fA_D6rNjL4N';
 
